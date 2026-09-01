72754ad3d5d185320d991f864b3bd436d8dbbb94
MiaYuDAYTOY <yyu61965@gmail>
2026-09-01 16:17:11 +0800

    left results

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ token_length = int(sys.argv[2])
 shadow_model = sys.argv[3]
 target_model = sys.argv[4]
 
-ATTACK_SEED = [3,4,5]
-TRAIN_NUMS = [3,5]
-PREFIX_LENGTHS = [32]
+ATTACK_SEED = [3]
+TRAIN_NUMS = [5]
+PREFIX_LENGTHS = [16]
 test_num = 20
 
 dataFactory = DataFactory()
